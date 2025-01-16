d5df4a5b39ff93e389a8262ae632a573e8fb6d1a
Sanjit Ghosh <sanjitkumarghosh0@gmail.com>
2025-01-16 06:39:41 +0600

    integrate pet adoption request send

--- src/components/PetDetailsCard/PetDetailsCard.jsx
+++ src/components/PetDetailsCard/PetDetailsCard.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import { format } from "date-fns";
 import "./textCss.css";
@@ -11,7 +11,9 @@ import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import { useForm } from "react-hook-form";
 import useAuth from "../../hook/useAuth";
-import { TextField, Tooltip } from "@mui/material";
+import { Tooltip } from "@mui/material";
+import useAxiosSecure from "../../hook/UseAxiosSecure";
+import Swal from "sweetalert2";
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   "& .MuiDialogContent-root": {
@@ -24,6 +26,15 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 
 const PetDetailsCard = ({ item }) => {
   const { user } = useAuth();
+  const [open, setOpen] = useState(false);
+  const axiosSecure = useAxiosSecure();
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const {
     name,
     image,
@@ -35,13 +46,14 @@ const PetDetailsCard = ({ item }) => {
     pet_owner_description,
     _id,
   } = item;
-  const [open, setOpen] = React.useState(false);
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
-  const handleClose = () => {
-    setOpen(false);
-  };
+
+  //   react hook form code
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   //   modify the pet description
   const cleanedDescription = useMemo(() => {
@@ -51,24 +63,35 @@ const PetDetailsCard = ({ item }) => {
     return "";
   }, [pet_description]);
 
-  //   modal form code
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const adoptData = {
-        petId: _id,
-        petName: name,
-        petImage: image,
-        userName: data?.userName,
-        userEmail: data?.userEmail,
-        userPhone: data?.userPhone,
-        userAddress: data?.userAddress,
+      petId: _id,
+      petName: name,
+      petImage: image,
+      userName: data?.userName,
+      userEmail: data?.userEmail,
+      userPhone: data?.userPhone,
+      userAddress: data?.userAddress,
+    };
+
+    try {
+      // submit data in db
+      const { data } = await axiosSecure.post("/adoptionRequest", adoptData);
+      console.log(data);
+      if (data.insertedId) {
+        Swal.fire({
+          position: "top-center",
+          icon: "success",
+          title: "Your request has been send!",
+          showConfirmButton: false,
+          timer: 1000,
+        });
+      }
+    } catch (err) {
+    } finally {
+      handleClose();
+      reset();
     }
-    console.log(adoptData);
   };
 
   return (
@@ -135,117 +158,117 @@ const PetDetailsCard = ({ item }) => {
           alt={name}
         />
       </div>
-      <React.Fragment>
-        <BootstrapDialog
-          onClose={handleClose}
-          aria-labelledby="customized-dialog-title"
-          open={open}
-          sx={{
-            "& .MuiDialog-paper": {
-              width: "800px",
-              maxWidth: "90vw",
-            },
-          }}
+
+      <BootstrapDialog
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+        sx={{
+          "& .MuiDialog-paper": {
+            width: "800px",
+            maxWidth: "90vw",
+          },
+        }}
+      >
+        <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+          {name}
+        </DialogTitle>
+        <IconButton
+          aria-label="close"
+          onClick={handleClose}
+          sx={(theme) => ({
+            position: "absolute",
+            right: 8,
+            top: 8,
+            color: theme.palette.grey[500],
+          })}
         >
-          <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
-            {name}
-          </DialogTitle>
-          <IconButton
-            aria-label="close"
-            onClick={handleClose}
-            sx={(theme) => ({
-              position: "absolute",
-              right: 8,
-              top: 8,
-              color: theme.palette.grey[500],
-            })}
-          >
-            <CloseIcon />
-          </IconButton>
-          <DialogContent>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="flex flex-col gap-5 mb-5">
-                {/* petId */}
-                <p>
-                  <strong>Pet Id:</strong> {_id}
-                </p>
+          <CloseIcon />
+        </IconButton>
+        <DialogContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-5 mb-5">
+              {/* petId */}
+              <p>
+                <strong>Pet Id:</strong> {_id}
+              </p>
 
-                {/* petName */}
-                <p>
-                  <strong>Pet Name:</strong> {name}
-                </p>
+              {/* petName */}
+              <p>
+                <strong>Pet Name:</strong> {name}
+              </p>
 
-                {/* petImg */}
-                <p className="flex items-center gap-2">
-                  <strong>Pet Image:</strong> <img src={image} alt="pet-img" className="w-10 h-10" />
-                </p>
-                {/* user name */}
-                <div>
-                  <Tooltip title="You can't edit your name!" placement="top">
-                    <input
-                      type="text"
-                      defaultValue={user?.displayName}
-                      {...register("userName", { required: true })}
-                      className="w-full border p-2 focus:outline focus:outline-0"
-                      readOnly
-                    />
-                  </Tooltip>
-                </div>
-                {/* user email */}
-                <div>
-                  <Tooltip title="You can't edit your email!" placement="top">
-                    <input
-                      type="text"
-                      defaultValue={user?.email}
-                      {...register("userEmail", { required: true })}
-                      className="w-full border p-2 focus:outline focus:outline-0"
-                      readOnly
-                    />
-                  </Tooltip>
-                </div>
-                {/* user phone */}
-                <div>
-                  <input
-                    type="number"
-                    placeholder="Phone Number"
-                    {...register("userPhone", { required: true })}
-                    className="w-full border p-2 focus:outline focus:outline-0"
-                  />
-                  {errors.userPhone && (
-                    <p className="text-red-500">This field is required</p>
-                  )}
-                </div>
-                {/* user address */}
-                <div>
+              {/* petImg */}
+              <p className="flex items-center gap-2">
+                <strong>Pet Image:</strong>{" "}
+                <img src={image} alt="pet-img" className="w-10 h-10" />
+              </p>
+              {/* user name */}
+              <div>
+                <Tooltip title="You can't edit your name!" placement="top">
                   <input
                     type="text"
-                    placeholder="Address"
-                    {...register("userAddress", { required: true })}
-                    className="w-full border p-2 focus:outline focus:outline-0 "
+                    defaultValue={user?.displayName}
+                    {...register("userName", { required: true })}
+                    className="w-full border p-2 focus:outline focus:outline-0"
+                    readOnly
                   />
-                  {errors.userAddress && (
-                    <p className="text-red-500">This field is required</p>
-                  )}
-                </div>
+                </Tooltip>
               </div>
-              <Button
-                type="submit"
-                variant="outlined"
-                sx={{
-                  backgroundColor: "#E16F52",
-                  border: "none",
-                  color: "white",
-                  "&:hover": {
-                    backgroundColor: "#D05A2B",
-                  },
-                }}
-              >
-                submit
-              </Button>
-            </form>
-          </DialogContent>
-        </BootstrapDialog>
-      </React.Fragment>
+              {/* user email */}
+              <div>
+                <Tooltip title="You can't edit your email!" placement="top">
+                  <input
+                    type="text"
+                    defaultValue={user?.email}
+                    {...register("userEmail", { required: true })}
+                    className="w-full border p-2 focus:outline focus:outline-0"
+                    readOnly
+                  />
+                </Tooltip>
+              </div>
+              {/* user phone */}
+              <div>
+                <input
+                  type="number"
+                  placeholder="Phone Number"
+                  {...register("userPhone", { required: true })}
+                  className="w-full border p-2 focus:outline focus:outline-0"
+                />
+                {errors.userPhone && (
+                  <p className="text-red-500">This field is required</p>
+                )}
+              </div>
+              {/* user address */}
+              <div>
+                <input
+                  type="text"
+                  placeholder="Address"
+                  {...register("userAddress", { required: true })}
+                  className="w-full border p-2 focus:outline focus:outline-0 "
+                />
+                {errors.userAddress && (
+                  <p className="text-red-500">This field is required</p>
+                )}
+              </div>
+            </div>
+            <Button
+              type="submit"
+              variant="outlined"
+              sx={{
+                backgroundColor: "#E16F52",
+                border: "none",
+                color: "white",
+                "&:hover": {
+                  backgroundColor: "#D05A2B",
+                },
+              }}
+            >
+              submit
+            </Button>
+          </form>
+        </DialogContent>
+      </BootstrapDialog>
     </>
   );
 };
